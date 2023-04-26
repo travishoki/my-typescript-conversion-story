b0a5c70b0971d0db62871fc0bc1c40b4a52fde59
Travis Hoki <travis.hoki@gmail.com>
2023-04-25 23:16:36 -0600

    update examples

---
+++ src/examples/2-unexpected-type.js
@@ -0,0 +1,15 @@
+export const Capitalize = (str) => {
+  if (typeof str !== "string") {
+    /*
+    Has to handle an unexpected type
+    1. Throw error
+
+    throw new Error("invalid string");
+    2. Return a defaulted value
+
+    return undefined;
+    */
+  }
+
+  return str.toUpperCase();
+};
---
+++ src/examples/2-unexpected-type.ts
@@ -0,0 +1,8 @@
+export const Capitalize = (str: string) => str.toUpperCase();
+
+/*
+Response:
+I like that!
+You know what you're going to get as props!
+No need to write a unit test for unexpected types
+*/
